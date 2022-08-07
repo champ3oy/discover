4f8e7ca5d42ed8e9e3c54dbed81370980cb38c3d
champ3oy <akotosel6@gmail.com>
2022-08-07 12:05:58 +0000

    fix: loading screen

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,53 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import React, { useState, useEffect } from "react";
+import styles from "../styles/Loading.module.scss";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [loading, setloading] = useState(true);
+  const [width, setwidth] = useState("");
+
+  useEffect(() => {
+    AOS.init({
+      offset: 300,
+    });
+
+    if (typeof window !== "undefined") {
+      let width_ =
+        document.body.clientWidth ||
+        window.innerHeight ||
+        document.documentElement.clientHeigh;
+
+      setwidth(width_);
+    }
+
+    setTimeout(() => {
+      setloading(false);
+    }, 3000);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className={styles.hero2imgs}>
+        <img
+          data-aos="fade-right"
+          className={styles.heroleft}
+          src="l2.png"
+          alt="hero"
+        />
+
+        <img
+          data-aos="fade-left"
+          className={styles.heroright}
+          src="l1.png"
+          alt="hero"
+        />
+      </div>
+    );
+  }
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
